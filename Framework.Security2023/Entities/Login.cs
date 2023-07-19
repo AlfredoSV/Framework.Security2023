4ba5a2b3f1db7ae592c27ae9c1b9eb682c2d8b80
Alfredo Sánchez <45853187+AlfredoSV@users.noreply.github.com>
2023-07-19 17:31:43 -0600

    Add project files.

---
+++ Framework.Security2023/Entities/Login.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Framework.Security2023.Entities
+{
+    public class Login
+    {
+        public enum StatusLogin
+        {
+            Ok = 1,
+            NoValid = -1
+
+        }
+        private string _User;
+        public string User
+        {
+            get { return _User; }
+            set { _User = value; }
+        }
+
+        private string _Password;
+        public string Password
+        {
+            get { return _Password; }
+            set { _Password = value; }
+        }
+
+        private StatusLogin _StatusLogin;
+        public StatusLogin StatusLog
+        {
+            get { return _StatusLogin; }
+            set { _StatusLogin = value; }
+        }
+
+        private Login(string user, string password, StatusLogin status)
+        {
+            this.User = user;
+            this.StatusLog = status;
+            this.Password = password;
+        }
+
+        public static Login Create(string user, string password, StatusLogin status = StatusLogin.NoValid)
+        {
+            return new Login(user, password, status);
+        }
+    }
+
+}
---
+++ Framework.Security2023/ServiceLogin.cs
@@ -0,0 +1,17 @@
+﻿using Framework.Security2023.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Framework.Security2023
+{
+    public class ServiceLogin : IServiceLogin
+    {
+        public Login Login(Login login)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
