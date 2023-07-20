5c4516ba002ed10f3a386c8566db04a5d7272d98
Alfredo Sánchez <45853187+AlfredoSV@users.noreply.github.com>
2023-07-19 19:10:26 -0600

    Add Scripts Table Fkw.sql and add RepositorieLogin.cs

---
+++ Framework.Security2023/Repositories/RespositorieLogin.cs
@@ -0,0 +1,19 @@
+﻿using Framework.Security2023.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Framework.Security2023.Repositories
+{
+    public class RespositorieLogin
+    {
+        public Login GetUser()
+        {
+
+
+            return null;
+        }
+    }
+}
---
+++ Framework.Security2023/TablesSql/Scripts Table Fkw.sql
@@ -0,0 +1,18 @@
+﻿--Scripts Tables Framework
+
+CREATE DATABASE Framework_Users;
+
+Go
+
+USE Framework_Users;
+
+GO
+
+CREATE TABLE Users(
+Id UNIQUEIDENTIFIER, 
+UserName VARCHAR(MAX),
+Password VARCHAR(MAX),
+DateCreated Datetime,
+UserCreated UNIQUEIDENTIFIER,
+LoginSessions INTEGER,
+UserBlocked bit);
