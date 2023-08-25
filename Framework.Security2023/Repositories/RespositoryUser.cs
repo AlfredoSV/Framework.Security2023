ef99160d25244b8a27bb737ebc18a77d47eea6bb
Alfredo Sánchez <45853187+AlfredoSV@users.noreply.github.com>
2023-08-25 09:02:53 -0600

    Modify the Delete method on RepositoryUser

--- Framework.Security2023/Repositories/RespositoryUser.cs
+++ Framework.Security2023/Repositories/RespositoryUser.cs
@@ -8,150 +8,156 @@ using System.Threading.Tasks;
 
 namespace Framework.Security2023.Repositories
 {
-    public class RespositoryUser
-    {
-        private SqlConnection _sqlConnection;
-        private SqlDataReader _sqlDataReader;
-        private SqlCommand _sqlCommand;
-        private readonly string _sqlTextConnection;
+	public class RespositoryUser
+	{
+		private SqlConnection _sqlConnection;
+		private SqlDataReader _sqlDataReader;
+		private SqlCommand _sqlCommand;
+		private readonly string _sqlTextConnection;
 
-        public RespositoryUser(){
-            this._sqlTextConnection = SlqConnectionStr.Instance.SqlConnectionString;
-        }
+		public RespositoryUser()
+		{
+			this._sqlTextConnection = SlqConnectionStr.Instance.SqlConnectionString;
+		}
 
-        public UserFkw GetUser(string userName)
-        {
-            UserFkw userResult = null;
-            string sqlGetUser = @"Select Id, UserName, Password, DateCreated, UserCreated, LoginSessions, UserBlocked,
+		public UserFkw GetUser(string userName)
+		{
+			UserFkw userResult = null;
+			string sqlGetUser = @"Select Id, UserName, Password, DateCreated, UserCreated, LoginSessions, UserBlocked,
                                 ApplyToken, RolId from UserFkw where UserName = @userName;";
-            this._sqlCommand = new SqlCommand();
-            using (this._sqlConnection = new SqlConnection(this._sqlTextConnection))
-            {
-                this._sqlCommand = new SqlCommand();
-                this._sqlCommand.Connection = this._sqlConnection;
-                this._sqlConnection.Open();
-                this._sqlCommand.CommandText = sqlGetUser;
-                this._sqlCommand.Parameters.AddWithValue("userName", userName);
-                this._sqlDataReader = this._sqlCommand.ExecuteReader();
+			this._sqlCommand = new SqlCommand();
+			using (this._sqlConnection = new SqlConnection(this._sqlTextConnection))
+			{
+				this._sqlCommand = new SqlCommand();
+				this._sqlCommand.Connection = this._sqlConnection;
+				this._sqlConnection.Open();
+				this._sqlCommand.CommandText = sqlGetUser;
+				this._sqlCommand.Parameters.AddWithValue("userName", userName);
+				this._sqlDataReader = this._sqlCommand.ExecuteReader();
 
-                if (this._sqlDataReader.HasRows)
-                {
-                    this._sqlDataReader.Read();
+				if (this._sqlDataReader.HasRows)
+				{
+					this._sqlDataReader.Read();
 
-                    userResult = UserFkw.Create(this._sqlDataReader.GetGuid(0),
-                        this._sqlDataReader.GetString(1), this._sqlDataReader.GetString(2),
-                        this._sqlDataReader.GetDateTime(3), this._sqlDataReader.GetGuid(4),
-                        this._sqlDataReader.GetInt32(5), this._sqlDataReader.GetBoolean(6),
+					userResult = UserFkw.Create(this._sqlDataReader.GetGuid(0),
+						this._sqlDataReader.GetString(1), this._sqlDataReader.GetString(2),
+						this._sqlDataReader.GetDateTime(3), this._sqlDataReader.GetGuid(4),
+						this._sqlDataReader.GetInt32(5), this._sqlDataReader.GetBoolean(6),
 						this._sqlDataReader.GetBoolean(7), this._sqlDataReader.GetGuid(8));
- 
-                }
-  
-            }
 
-            return userResult;
-        }
+				}
 
-        public int Save(UserFkw newUser)
-        {
+			}
 
-            int result;
-            string sqlGetUser = "INSERT INTO UserFkw VALUES(@id, @userName, @password, @dateCreated, @userCreated, @loginSessions,@rolId, @applyToken, @userBlocked);";
-            this._sqlCommand = new SqlCommand();
-            using (this._sqlConnection = new SqlConnection(this._sqlTextConnection))
-            {
-                this._sqlCommand = new SqlCommand();
-                this._sqlCommand.Connection = this._sqlConnection;
-                this._sqlConnection.Open();
-                this._sqlCommand.CommandText = sqlGetUser;
-                this._sqlCommand.Parameters.AddWithValue("Id", newUser.Id);
-                this._sqlCommand.Parameters.AddWithValue("userName", newUser.UserName);
-                this._sqlCommand.Parameters.AddWithValue("password", newUser.UserName);
-                this._sqlCommand.Parameters.AddWithValue("dateCreated", newUser.DateCreated);
-                this._sqlCommand.Parameters.AddWithValue("userCreated", newUser.UserCreated);
-                this._sqlCommand.Parameters.AddWithValue("loginSessions", newUser.LoginSessions);
-                this._sqlCommand.Parameters.AddWithValue("userBlocked", newUser.UserBlocked);
+			return userResult;
+		}
+
+		public int Save(UserFkw newUser)
+		{
+
+			int result;
+			string sqlGetUser = "INSERT INTO UserFkw VALUES(@id, @userName, @password, @dateCreated, @userCreated, @loginSessions,@rolId, @applyToken, @userBlocked);";
+			this._sqlCommand = new SqlCommand();
+			using (this._sqlConnection = new SqlConnection(this._sqlTextConnection))
+			{
+				this._sqlCommand = new SqlCommand();
+				this._sqlCommand.Connection = this._sqlConnection;
+				this._sqlConnection.Open();
+				this._sqlCommand.CommandText = sqlGetUser;
+				this._sqlCommand.Parameters.AddWithValue("Id", newUser.Id);
+				this._sqlCommand.Parameters.AddWithValue("userName", newUser.UserName);
+				this._sqlCommand.Parameters.AddWithValue("password", newUser.UserName);
+				this._sqlCommand.Parameters.AddWithValue("dateCreated", newUser.DateCreated);
+				this._sqlCommand.Parameters.AddWithValue("userCreated", newUser.UserCreated);
+				this._sqlCommand.Parameters.AddWithValue("loginSessions", newUser.LoginSessions);
+				this._sqlCommand.Parameters.AddWithValue("userBlocked", newUser.UserBlocked);
 				this._sqlCommand.Parameters.AddWithValue("rolId", newUser.RolId);
 				this._sqlCommand.Parameters.AddWithValue("applyToken", newUser.ApplyToken);
 
-				result =  this._sqlCommand.ExecuteNonQuery();
+				result = this._sqlCommand.ExecuteNonQuery();
 
-            }
+			}
 
-            return result;
+			return result;
 
-        }
+		}
 
-        public bool Delete(Guid userId)
-        {
+		public bool Delete(Guid userId)
+		{
 
-            bool deleteUserFkw, deleteUserInfo, deleteUserTokens;            
-            this._sqlCommand = new SqlCommand();
+			bool deleteUserFkw, deleteUserInfo, deleteUserTokens;
+			this._sqlCommand = new SqlCommand();
 			SqlTransaction sqlTransaction = null;
 
 			try
-            {
-				using (this._sqlConnection = new SqlConnection(this._sqlTextConnection))
-				{
-					sqlTransaction = this._sqlConnection.BeginTransaction();			
-					this._sqlCommand = new SqlCommand();
-					this._sqlCommand.Connection = this._sqlConnection;
-					this._sqlCommand.Transaction = sqlTransaction;
-					this._sqlConnection.Open();
+			{
+				this._sqlConnection = new SqlConnection(this._sqlTextConnection);
 
 
-					this._sqlCommand.CommandText = "DELETE FROM UserFkw WHERE Id = @Id";
-					this._sqlCommand.Parameters.AddWithValue("Id", userId);
-					deleteUserFkw = this._sqlCommand.ExecuteNonQuery() > 0;
+				this._sqlConnection.Open();
+				sqlTransaction = this._sqlConnection.BeginTransaction();
+				this._sqlCommand = new SqlCommand();
+				this._sqlCommand.Connection = this._sqlConnection;
+				this._sqlCommand.Transaction = sqlTransaction;
 
-					this._sqlCommand.CommandText = "DELETE FROM UserInformation WHERE idUser = @Id";
-					this._sqlCommand.Parameters.AddWithValue("Id", userId);
-					deleteUserInfo = this._sqlCommand.ExecuteNonQuery() > 0;
+				this._sqlCommand.CommandText = "DELETE FROM UserFkw WHERE Id = @Id";
+				this._sqlCommand.Parameters.AddWithValue("Id", userId);
+				deleteUserFkw = this._sqlCommand.ExecuteNonQuery() > 0;
 
-					this._sqlCommand.CommandText = "DELETE FROM UserToken WHERE id = @Id";
-					this._sqlCommand.Parameters.AddWithValue("Id", userId);
-					deleteUserTokens = this._sqlCommand.ExecuteNonQuery() > 0;
 
-					sqlTransaction.Commit();
+				this._sqlCommand.CommandText = "DELETE FROM UserInformation WHERE idUser = @Id";
+				deleteUserInfo = this._sqlCommand.ExecuteNonQuery() > 0;
 
-				}
+
+				this._sqlCommand.CommandText = "DELETE FROM UserToken WHERE id = @Id";
+				deleteUserTokens = this._sqlCommand.ExecuteNonQuery() > 0;
+
+				sqlTransaction.Commit();
+
+
 			}
-            catch (Exception e)
-            {
-                sqlTransaction.Rollback();
-                throw e;
-            }         
+			catch (Exception e)
+			{
+				sqlTransaction.Rollback();
+				
+				throw e;
+			}
+			finally
+			{
+				this._sqlConnection.Close();
+			}
 
-            return deleteUserFkw || deleteUserInfo || deleteUserTokens;
+			return deleteUserFkw || deleteUserInfo || deleteUserTokens;
 
-        }
+		}
 
-        public int UpdatePassword(Guid userId, string newPassword)
-        {
+		public int UpdatePassword(Guid userId, string newPassword)
+		{
 
-            int result;
-            string sqlGetUser = "UPDATE Users SET Password = @password WHERE Id = @id";
-            this._sqlCommand = new SqlCommand();
-            using (this._sqlConnection = new SqlConnection(this._sqlTextConnection))
-            {
-                this._sqlCommand = new SqlCommand();
-                this._sqlCommand.Connection = this._sqlConnection;
-                this._sqlConnection.Open();
-                this._sqlCommand.CommandText = sqlGetUser;
-                this._sqlCommand.Parameters.AddWithValue("id", userId);
-                this._sqlCommand.Parameters.AddWithValue("password", newPassword);
-                result = this._sqlCommand.ExecuteNonQuery();
+			int result;
+			string sqlGetUser = "UPDATE Users SET Password = @password WHERE Id = @id";
+			this._sqlCommand = new SqlCommand();
+			using (this._sqlConnection = new SqlConnection(this._sqlTextConnection))
+			{
+				this._sqlCommand = new SqlCommand();
+				this._sqlCommand.Connection = this._sqlConnection;
+				this._sqlConnection.Open();
+				this._sqlCommand.CommandText = sqlGetUser;
+				this._sqlCommand.Parameters.AddWithValue("id", userId);
+				this._sqlCommand.Parameters.AddWithValue("password", newPassword);
+				result = this._sqlCommand.ExecuteNonQuery();
 
-            }
+			}
 
-            return result;
+			return result;
 
-        }
+		}
 
-        public int Update(UserFkw newUser)
-        {
+		public int Update(UserFkw newUser)
+		{
 
-            int result;
-            string sqlGetUser = @"
+			int result;
+			string sqlGetUser = @"
              UPDATE dbo.Users
              SET 
 	           UserName = @userName
@@ -162,28 +168,28 @@ namespace Framework.Security2023.Repositories
               ,UserBlocked = @userBlocked
              WHERE Id = @Id;
             ";
-            this._sqlCommand = new SqlCommand();
-            using (this._sqlConnection = new SqlConnection(this._sqlTextConnection))
-            {
-                this._sqlCommand = new SqlCommand();
-                this._sqlCommand.Connection = this._sqlConnection;
-                this._sqlConnection.Open();
-                this._sqlCommand.CommandText = sqlGetUser;
-                this._sqlCommand.Parameters.AddWithValue("Id", newUser.Id);
-                this._sqlCommand.Parameters.AddWithValue("userName", newUser.UserName);
-                this._sqlCommand.Parameters.AddWithValue("password", newUser.UserName);
-                this._sqlCommand.Parameters.AddWithValue("dateCreated", newUser.DateCreated);
-                this._sqlCommand.Parameters.AddWithValue("userCreated", newUser.UserCreated);
-                this._sqlCommand.Parameters.AddWithValue("loginSessions", newUser.LoginSessions);
-                this._sqlCommand.Parameters.AddWithValue("userBlocked", newUser.UserBlocked);
-                result = this._sqlCommand.ExecuteNonQuery();
+			this._sqlCommand = new SqlCommand();
+			using (this._sqlConnection = new SqlConnection(this._sqlTextConnection))
+			{
+				this._sqlCommand = new SqlCommand();
+				this._sqlCommand.Connection = this._sqlConnection;
+				this._sqlConnection.Open();
+				this._sqlCommand.CommandText = sqlGetUser;
+				this._sqlCommand.Parameters.AddWithValue("Id", newUser.Id);
+				this._sqlCommand.Parameters.AddWithValue("userName", newUser.UserName);
+				this._sqlCommand.Parameters.AddWithValue("password", newUser.UserName);
+				this._sqlCommand.Parameters.AddWithValue("dateCreated", newUser.DateCreated);
+				this._sqlCommand.Parameters.AddWithValue("userCreated", newUser.UserCreated);
+				this._sqlCommand.Parameters.AddWithValue("loginSessions", newUser.LoginSessions);
+				this._sqlCommand.Parameters.AddWithValue("userBlocked", newUser.UserBlocked);
+				result = this._sqlCommand.ExecuteNonQuery();
 
-            }
+			}
 
-            return result;
+			return result;
 
-        }
+		}
 
 
-    }
+	}
 }
