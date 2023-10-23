db2f67d3960f5eb3c9d3e2b5a2deb48b69293b78
Alfredo Sánchez <45853187+AlfredoSV@users.noreply.github.com>
2023-10-23 16:07:10 -0600

    Wash added RepositoryChangePasswordRequest class

---
+++ Framework.Security2023/Entities/ChangePasswordRequest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Framework.Security2023.Entities
+{
+    class ChangePasswordRequest
+    {
+        public Guid IdRequest { get; set; }
+        public Guid UserId { get; set; }
+        public DateTime DateExpired { get; set; }
+        public DateTime DateCreated { get; set; }
+
+        private ChangePasswordRequest(Guid idRequest, Guid userId, DateTime dateExpired, DateTime dateCreated)
+        {
+            IdRequest = idRequest;
+            UserId = userId;
+            DateExpired = dateExpired;
+            DateCreated = dateCreated;
+        }
+
+        public static ChangePasswordRequest Create(Guid idRequest, Guid userId, DateTime dateExpired, DateTime dateCreated)
+        {
+            return new ChangePasswordRequest(idRequest, userId, dateExpired, dateCreated);
+        }
+
+    }
+}
---
+++ Framework.Security2023/Repositories/RepositoryChangePasswordRequest.cs
@@ -0,0 +1,73 @@
+﻿using Framework.Security2023.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Framework.Security2023.Repositories
+{
+    class RepositoryChangePasswordRequest
+    {
+        private readonly string _sqlStr;
+
+        RepositoryChangePasswordRequest()
+        {
+            _sqlStr = SlqConnectionStr.Instance.SqlConnectionString;
+        }
+
+        void Save(ChangePasswordRequest changePasswordRequest)
+        {
+            string insertStr = @"INSERT INTO ChangePasswordRequest VALUES(newId(),
+            @userId,@dateExpired,@dateCreated)";
+
+            using (SqlConnection sqlConnection = new SqlConnection(_sqlStr))
+            {
+                sqlConnection.Open();
+                SqlCommand cmd = sqlConnection.CreateCommand();
+                cmd.CommandText = insertStr;
+                cmd.Parameters.AddWithValue("userId", changePasswordRequest.UserId);
+                cmd.Parameters.AddWithValue("dateExpired", changePasswordRequest.DateExpired);
+                cmd.Parameters.AddWithValue("dateCreated", changePasswordRequest.DateCreated);
+
+                cmd.ExecuteNonQuery();
+
+
+            }
+
+
+        }
+
+        ChangePasswordRequest SelectByIdRequest(Guid idRequest)
+        {
+            string insertStr = @"Select * from ChangePasswordRequest where IdRequest = @idRequest";
+            ChangePasswordRequest changePasswordRequest = null;
+            SqlDataReader sqlDataReader;
+            using (SqlConnection sqlConnection = new SqlConnection(_sqlStr))
+            {
+                sqlConnection.Open();
+                SqlCommand cmd = sqlConnection.CreateCommand();
+                cmd.CommandText = insertStr;
+                cmd.Parameters.AddWithValue("idRequest", idRequest);
+
+                sqlDataReader = cmd.ExecuteReader();
+
+                if (sqlDataReader.HasRows)
+                {
+                    sqlDataReader.Read();
+
+                    changePasswordRequest = ChangePasswordRequest.Create(
+                        sqlDataReader.GetGuid(0), sqlDataReader.GetGuid(1),
+                        sqlDataReader.GetDateTime(2), sqlDataReader.GetDateTime(3));
+
+                }
+
+            }
+
+            return changePasswordRequest;
+
+
+        }
+    }
+}
